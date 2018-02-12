3cb6ce0b78284c5e4b05b98a531508cb60920512
glucena <gabriellucenatoro@gmail.com>
2018-02-12 21:34:12 +0100

    Home styled a bit

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,24 @@
 <template>
-  <header>
-    <h1>{{ qrId }}</h1>
-  </header>
+  <div>
+    <header>
+      <h1>Hello {{ qrId }}!</h1>
+    </header>
+    <div class="content">
+      <md-card>
+        <md-card-media-cover md-solid>
+          <md-card-media md-ratio="4:3">
+            <img src="/assets/examples/card-sky.jpg" alt="Skyscraper">
+          </md-card-media>
+          <md-card-area>
+            <md-card-header>
+              <span class="md-title">Solid background</span>
+              <span class="md-subhead">4/3 image</span>
+            </md-card-header>
+          </md-card-area>
+        </md-card-media-cover>
+      </md-card>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -12,7 +29,6 @@ export default {
     }
   },
   created: function () {
-    debugger
     this.qrId = this.$route.params.qrId
   }
 }
@@ -25,8 +41,12 @@ header {
   margin-top: -24px;
   background: linear-gradient(141deg, #0fb8ad 0%, #1fc8db 51%, #2cb5e8 75%);
 }
+h1 {
+  font-size: 4vh;
+}
 
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
 }
 
@@ -41,6 +61,6 @@ li {
 }
 
 a {
-  color: #35495E;
+  color: #35495e;
 }
 </style>
