490869cef5590b1bcaae61673814e60916b5c9b2
glucena <gabriellucenatoro@gmail.com>
2018-02-13 18:56:47 +0100

    Material design added

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -6,13 +6,55 @@
     <div class="content">
       <md-card>
         <md-card-media-cover md-solid>
-          <md-card-media md-ratio="4:3">
-            <img src="/assets/examples/card-sky.jpg" alt="Skyscraper">
+          <md-card-media md-ratio="16:9">
+            <img src="../assets/card-sky.jpg" alt="Skyscraper">
           </md-card-media>
           <md-card-area>
             <md-card-header>
               <span class="md-title">Solid background</span>
-              <span class="md-subhead">4/3 image</span>
+              <span class="md-subhead">16/9 image</span>
+            </md-card-header>
+          </md-card-area>
+        </md-card-media-cover>
+      </md-card>
+      
+      <md-card>
+        <md-card-media-cover md-solid>
+          <md-card-media md-ratio="16:9">
+            <img src="../assets/card-sky.jpg" alt="Skyscraper">
+          </md-card-media>
+          <md-card-area>
+            <md-card-header>
+              <span class="md-title">Solid background</span>
+              <span class="md-subhead">16/9 image</span>
+            </md-card-header>
+          </md-card-area>
+        </md-card-media-cover>
+      </md-card>
+
+      <md-card>
+        <md-card-media-cover md-solid>
+          <md-card-media md-ratio="16:9">
+            <img src="../assets/card-sky.jpg" alt="Skyscraper">
+          </md-card-media>
+          <md-card-area>
+            <md-card-header>
+              <span class="md-title">Solid background</span>
+              <span class="md-subhead">16/9 image</span>
+            </md-card-header>
+          </md-card-area>
+        </md-card-media-cover>
+      </md-card>
+      
+      <md-card>
+        <md-card-media-cover md-solid>
+          <md-card-media md-ratio="16:9">
+            <img src="../assets/card-sky.jpg" alt="Skyscraper">
+          </md-card-media>
+          <md-card-area>
+            <md-card-header>
+              <span class="md-title">Solid background</span>
+              <span class="md-subhead">16/9 image</span>
             </md-card-header>
           </md-card-area>
         </md-card-media-cover>
--- src/main.js
+++ src/main.js
@@ -4,7 +4,12 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+import { MdCard } from 'vue-material/dist/components'
+import 'vue-material/dist/vue-material.min.css'
+
 Vue.config.productionTip = false
+
+Vue.use(MdCard)
 
 /* eslint-disable no-new */
 new Vue({
